d37f59fdeb1d2f969e6a5abcf0ba480d428628b6
wilson496 <cw4886130@gmail.com>
2018-07-31 16:54:20 -0700

    fixed lambda function handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,13 @@
-def handle(event, context):
-	return("HELLO")
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+def lambda_handler(event, context):
+	
+	process = CrawlerProcess(get_project_settings())
+
+	# 'game' is the name of the spider of the project.
+	process.crawl('game')
+	process.start() # the script will block here until the crawling is finished
+
+	return((event, context)
