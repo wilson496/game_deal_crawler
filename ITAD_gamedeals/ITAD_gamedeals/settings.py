73d9952583ec68974523482bc22ac2b937a94ef2
Cameron Wilson <cameronwilson@Camerons-MacBook-Pro.local>
2021-03-14 17:08:41 -0700

    Create POST request and view for adding game model as record to game table

--- ITAD_gamedeals/ITAD_gamedeals/settings.py
+++ ITAD_gamedeals/ITAD_gamedeals/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'game',
+    #'game',
+    'game.apps.GameConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Uncomment this when deploying to prod
+'''
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+'''
