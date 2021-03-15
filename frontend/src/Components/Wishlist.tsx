10d83ec66d7933e2d59dc9f6c19166f9451f6e92
Charlie Friend <charles.d.friend@gmail.com>
2021-03-14 17:02:12 -0700

    Add promote/demote button

--- frontend/src/Components/Wishlist.tsx
+++ frontend/src/Components/Wishlist.tsx
@@ -17,24 +17,60 @@ type WishlistState = {
 
 export default class WishList extends React.Component<{}, WishlistState> {
 
-    componentWillMount() {
-        this.setState({
+    constructor(props: {}) {
+        super(props)
+
+        this.state = {
             items: [
                 { name: "Valheim", price: 4.99 },
                 { name: "Rust", price: 25.13 },
                 { name: "My Spicy Booty", price: 3.50 }
             ]
-        })
+        }
+    }
+
+    /**
+     * Increases the rank of an item in the list.
+     * @param index Index of the item to promote in this.state.items.
+     */
+    promoteItem(index: number) {
+        const items: WishListItem[] = this.state.items.slice()
+
+        // Can't promode an item with index > 1
+        if (index > 0) {
+            // Swap this element with the one above it
+            const toPromote = items[index]
+            items[index] = items[index - 1]
+            items[index - 1] = toPromote
+        }
+
+        this.setState({items: items})
+    }
+
+    /**
+     * Decreases the rank of an item in the list.
+     * @param index Index of the item to demote in this.state.items.
+     */
+    demoteItem(index: number) {
+        const items: WishListItem[] = this.state.items.slice()
+
+        if (index < items.length) {
+            // Swap this element with the one below
+            const toDemote = items[index]
+            items[index] = items[index + 1]
+            items[index + 1] = toDemote
+        }
+
+        this.setState({items: items})
     }
 
     render() {
 
-        let index = 1
-        const rows: JSX.Element[] = []
-        for (const item of this.state.items) {
-            rows.push(<WishlistItemRow rank={index} name={item.name} price={item.price}/>)
-            index = index + 1
-        }
+        const rows = this.state.items.map((item, index) => {
+            return <WishlistItemRow key={index} rank={index} name={item.name} price={item.price} 
+                        promoteItem={() => this.promoteItem(index)}
+                        demoteItem={() => this.demoteItem(index)}/>
+        })
 
         return (
             <div>
--- frontend/src/Components/WishlistItemRow.tsx
+++ frontend/src/Components/WishlistItemRow.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
 
 type WishlistItemProps = {
 
@@ -11,7 +12,13 @@ type WishlistItemProps = {
     name: string
 
     // Decimal number indicating price
-    price: number
+    price: number,
+
+    /** Function for promoting an item - func(index) should promote the item. */
+    promoteItem: Function
+
+    /** Function for demoting an item - func(index) should demote the item. */
+    demoteItem: Function
 
 }
 
@@ -21,7 +28,11 @@ export default class WishlistItemRow extends React.Component<WishlistItemProps> 
         return (
             <Row>
                 <Col sm={2} className="text-center">{this.props.rank}</Col>
-                <Col sm={7}>{this.props.name}</Col>
+                <Col sm={7}>
+                    {this.props.name}
+                    <Button onClick={() => this.props.promoteItem()}>Promote</Button>
+                    <Button onClick={() => this.props.demoteItem()}>Demote</Button>
+                </Col>
                 <Col sm={3}>${this.props.price.toFixed(2)}</Col>
             </Row>
         )
